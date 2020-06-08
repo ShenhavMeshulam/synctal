d653d2c38dd90c5bc29281764e221e366e745675
ShenhavMeshulam <snhvm0@gmail.com>
2020-06-08 22:50:10 +0300

    prototype SignIn

--- client/src/app/components/Login.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default ({ className }) => {
-    return (<div className={className}><h1>login page</h1></div>);
-};
--- client/src/app/components/Routes.js
+++ client/src/app/components/Routes.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
-import Login from './Login';
+import SignIn from './SignIn';
 
 export default () => {
     return (
         <Router>
             <Route exact path="/" component={App} />
-            <Route path="/login" component={Login} />
+            <Route path="/SignIn" component={SignIn} />
         </Router>
     );
 }
